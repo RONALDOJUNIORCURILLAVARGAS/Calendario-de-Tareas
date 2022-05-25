95b8f886a0424df89b34fc4eef0b3cf89b4d916a
RONALDOJUNIORCURILLAVARGAS <ronaldocurillavargas@gmail.com>
2022-05-25 07:49:44 -0500

    Segundo commit

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,5 +1,24 @@
 
 export default()=>({
     isLoading:true,
-    entries:[1,2,3,4,5,6,],
+    entries:[
+        {
+        id:new Date().getTime(),
+        date: new Date().toDateString(),
+        test:'Ullamco minim laboris consectetur consequat incididunt. Commodo amet exercitation labore minim cupidatat non cupidatat exercitation proident deserunt non et. Incididunt laborum laboris exercitation tempor esse ut consectetur anim sint nisi sint exercitation. Irure aliquip cupidatat commodo pariatur dolor dolor. Id voluptate Lorem enim minim. Laborum quis dolore labore sint do dolore esse.',
+        picture: null,    
+        },
+        {
+            id:new Date().getTime()+1000,
+            date: new Date().toDateString(),
+            test:'Qui officia elit nisi sint. Pariatur commodo deserunt consectetur anim ea duis commodo aute occaecat ea. Laboris incididunt ipsum quis tempor eu in ullamco tempor fugiat quis ad cupidatat proident nulla.',
+            picture: null,    
+        },
+        {
+            id:new Date().getTime()+2000,
+            date: new Date().toDateString(),
+            test:'Aliquip elit laboris culpa anim exercitation. Exercitation esse mollit voluptate officia do Lorem officia exercitation est. Qui excepteur ad eiusmod duis. Quis officia in minim Lorem quis voluptate.',
+            picture: null,    
+        },
+    ],
 })
